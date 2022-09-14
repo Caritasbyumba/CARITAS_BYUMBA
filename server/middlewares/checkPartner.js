e1da0f9a7f1be5ce11f27c2ad209f0e4389569ba
Pacifique78 <tuyipaccy78@gmail.com>
2022-09-14 12:25:41 +0200

    fix edit partners error

--- server/middlewares/checkPartner.js
+++ server/middlewares/checkPartner.js
@@ -4,9 +4,9 @@ import validationHelper from '../helpers/ValidationHelper.js';
 const checkPartner = (req, res, next) => {
   const partnerSchemas = Joi.object().keys({
     name: Joi.string().trim().max(70).required(),
-    enQuote: Joi.string().trim().max().required().label('English quote'),
-    frQuote: Joi.string().trim().max().required().label('French quote'),
-    rwQuote: Joi.string().trim().max().required().label('Kinyarwanda quote'),
+    enQuote: Joi.string().trim().required().label('English quote'),
+    frQuote: Joi.string().trim().required().label('French quote'),
+    rwQuote: Joi.string().trim().required().label('Kinyarwanda quote'),
     enDescription: Joi.string().trim().required().label('English description'),
     frDescription: Joi.string().trim().required().label('French description'),
     rwDescription: Joi.string()
