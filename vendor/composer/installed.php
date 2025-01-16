379eabc4417f5a73be60efbfea8b74c852ff349c
byumbacaritas@gmail.com <byumbacaritas@gmail.com>
2025-01-16 12:33:05 +0200

    Add basic project structure

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'caritasbyumba/website',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '017f0446feedfd210f050a713de5f3dd1ee5a796',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'caritasbyumba/website' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '017f0446feedfd210f050a713de5f3dd1ee5a796',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
